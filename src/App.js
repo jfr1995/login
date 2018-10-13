1ffd1b4f55de653390b3920c0ca1dcfa022dc234
Juan Ramirez <juanramirez1776@gmail.com>
2018-10-12 23:45:14 -0700

    test

--- src/App.js
+++ src/App.js
@@ -4,18 +4,35 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <FormContainer className="formContainer" />;
+    return (
+      <div>
+        <ContactContainer />
+        <FormContainer />
+      </div>
+    );
   }
 }
+
+const ContactContainer = () => {
+  return (
+    <div className="contact-container">
+      <ul className="contact-list">
+        <li>Email</li>
+        <li>Address</li>
+        <li>Phone number</li>
+      </ul>
+    </div>
+  );
+};
 
 const FormContainer = () => {
   return (
     <div>
       <form>
         <div className="form-text">Username</div>
-        <input type="text" name="user" />
+        <input type="text" className="input-fld" />
         <div className="form-text">Password</div>
-        <input type="text" name="user" />
+        <input type="text" className="input-fld" />
       </form>
     </div>
   );
